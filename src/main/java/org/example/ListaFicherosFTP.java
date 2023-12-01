eb9f726f7613a2203c4db3df7d30493416ba4a5e
alu10701951 <alugar1951@ieselcaminas.org>
2023-12-01 19:59:33 +0100

    Actividad 4.2

--- src/main/java/org/example/ListaFicherosFTP.java
+++ src/main/java/org/example/ListaFicherosFTP.java
@@ -1,6 +1,7 @@
 package org.example;
 
-import java.io.IOException;
+import java.io.*;
+
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 import org.apache.commons.net.ftp.FTPFile;
@@ -56,14 +57,6 @@ public class ListaFicherosFTP {
             System.out.printf("INFO: Directorio actual en servidor: %s, contenidos:\n", clienteFTP.printWorkingDirectory());
 
 
-            clienteFTP.makeDirectory("desdeJava"); //creamos una carpeta llamada "desdeJava"
-            clienteFTP.changeWorkingDirectory("desdeJava"); //entrar en la carpeta creada, llamada "desdeJava"
-
-            String contenido = "texto";
-
-
-
-
             FTPFile[] fichServ = clienteFTP.listFiles();
             for(FTPFile f: fichServ) {
                 String infoAdicFich = "";
@@ -76,7 +69,40 @@ public class ListaFicherosFTP {
 
                 System.out.printf("%s%s\n", f.getName(), infoAdicFich);
             }
+//--------------------------------------------Actividad4.2--------------------------------------------------------------
 
+            clienteFTP.makeDirectory("desdeJava"); //creamos una carpeta llamada "desdeJava"
+            clienteFTP.changeWorkingDirectory("desdeJava"); //entrar en la carpeta creada, llamada "desdeJava"
+
+            //almacenar un nuevo documento "desdeElPrograma.txt" con contenido "texto"
+            String contenido = "texto";
+            InputStream inputStream = new ByteArrayInputStream(contenido.getBytes());
+            clienteFTP.storeFile("desdeElPrograma.txt: ", inputStream);
+            inputStream.close();
+
+            //recupera el docuemtno almacenado y mostrar por pantalla su contenido
+            OutputStream outputStream = new ByteArrayOutputStream();
+            clienteFTP.retrieveFile("desdeElPrograma.txt", outputStream);
+            System.out.println("INFO: Contenido de desdeElPrograma.txt" + outputStream.toString());
+            outputStream.close();
+
+            //recorre los ficheros y directorios en la carpeta actual
+            System.out.println("INFO: Contenido de la carpeta actual:");
+            FTPFile[] ficheros = clienteFTP.listFiles();
+            for(FTPFile fichero: ficheros){
+                System.out.println(fichero.getName());
+            }
+
+            //vuelve al directorio padre
+            clienteFTP.changeToParentDirectory();
+
+            //vuelve a recorrer los ficheros y directorios en la carpeta actual
+            System.out.println("INFO: Contenidos de la carpeta actual despues de volver al directorio padre: ");
+            ficheros = clienteFTP.listFiles();
+            for (FTPFile fichero : ficheros){
+                System.out.println(fichero.getName());
+            }
+//----------------------------------------------------------------------------------------------------------------------
 
         } catch (IOException e) {
             System.out.println("ERROR: conectando al servidor");
